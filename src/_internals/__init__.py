9995eb3a6f4f9283956ebb14a4489bec3286573b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-04-10 21:06:56 -0500

    update

--- src/_internals/__init__.py
+++
@@ -1,3 +0,0 @@
-from .train_estimator import train_estimator
-
-__all__ = ["train_estimator"]
--- src/_internals/report.py
+++
@@ -1,9 +0,0 @@
-"""Report"""
-
-
-def report(estimator, mse, mae, r2):
-
-    print(estimator, ":", sep="")
-    print(f"  MSE: {mse}")
-    print(f"  MAE: {mae}")
-    print(f"  R2: {r2}")
